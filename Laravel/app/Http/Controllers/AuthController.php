07d2de9ed37bb384fc76c839c298e6443d1c925c
helingfeng <857190327@qq.com>
2018-12-20 18:00:36 +0800

    Tested: 测试用户登录 code2session

---
+++ Laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: helingfeng
+ */
+
+namespace App\Http\Controllers;
+
+
+class AuthController
+{
+    // https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+
+    public function code2session()
+    {
+
+    }
+}
