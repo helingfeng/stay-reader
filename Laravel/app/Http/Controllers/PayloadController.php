95fcff59dcaea3b685986f91608da08090c851f7
helingfeng <857190327@qq.com>
2019-01-14 14:17:48 +0800

    Changed: webhooks

--- Laravel/app/Http/Controllers/PayloadController.php
+++ Laravel/app/Http/Controllers/PayloadController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class PayloadController
@@ -18,6 +19,11 @@ class PayloadController
             $command = 'cd /home/wwwroot/StayReader/Laravel/public/hexo-blog && git pull && npm install -g && ./node_modules/.bin/hexo g';
             $process = new Process($command);
             $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+            return response()->json(['message' => $process->getOutput()]);
         }
         Log::info("remote request key :{$secret}");
         return response()->json(['message' => 'receive push event.']);
